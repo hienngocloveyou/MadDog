810234ceac27bf5e0be4dcc3c018f8da712d5568
Hien Ngoc Tran <hienngocloveyou@yahoo.com>
2021-10-11 20:33:13 +0700

    Update

--- MadDogSetting.cs
+++ MadDogSetting.cs
@@ -12,24 +12,33 @@ namespace MadDog
         public MadDogSetting() //test
         {
             Enable = new ToggleNode(true);
-            
+            Cameras = new UnitSettings(0x008000ff, 0);
+
         }
+
+        [Menu("Cameras", 1)]
+        public UnitSettings Cameras { get; set; }
     }
 
     public class UnitSettings : ISettings
     {
         public UnitSettings(uint color, uint outline)
         {
-            Enable = new ToggleNode(true);
             
-        }
+            Height = new RangeNode<float>(10, 10, 100);
+            Color = color;
+            Outline = outline;
+            BackGround = SharpDX.Color.Black;
 
-        public UnitSettings(uint color, uint outline, uint percentTextColor, bool showHealthText, int width, int height) : this(color, outline)
-        {
-           
         }
 
         public ToggleNode Enable { get; set; }
-        
+        public RangeNode<float> Height { get; set; }
+        public ColorNode Color { get; set; }
+        public ColorNode Outline { get; set; }
+        public ColorNode BackGround { get; set; }
+
+
+
     }
 }
