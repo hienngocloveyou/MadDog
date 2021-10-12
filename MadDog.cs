e04921dd41fec9761dbefe611c2789eff0742ea9
Hien Ngoc Tran <hienngocloveyou@yahoo.com>
2021-10-12 21:12:57 +0700

    test

--- MadDog.cs
+++ MadDog.cs
@@ -66,7 +66,7 @@ namespace MadDog
         {
 
             DrawEllipseToWorld(GetLocalPlayerPos(), Settings.Distance.distance.Value, 25, 2, Color.LawnGreen);
-            Settings.Distance.distance.Value = (int)GetLocalPlayerPos().X;
+            //Settings.Distance.distance.Value = (int)GetLocalPlayerPos().X;
 
         }
 
