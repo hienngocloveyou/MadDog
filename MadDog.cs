e5376e09de19952cf5143eaae921e6185b739c15
Hien Ngoc Tran <hienngocloveyou@yahoo.com>
2021-10-13 16:50:14 +0700

    test

--- MadDog.cs
+++ MadDog.cs
@@ -209,6 +209,7 @@ namespace MadDog
             {
                 if (_entities.Count > 0)
                 {
+                    /*
                     if (Input.IsKeyDown(1) || Input.IsKeyDown(2))
                     {
                         Input.KeyPressRelease(Settings.Activeskill.Value);
@@ -218,6 +219,8 @@ namespace MadDog
                     {
                         MonsterAim(_entities[0]);
                     }
+                    */
+                    MonsterAim(_entities[0]);
                 }
             }
             
