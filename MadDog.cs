e8cc1a2a85ec949f15276fb4921c369043690893
Hien Ngoc Tran <hienngocloveyou@yahoo.com>
2021-10-16 16:58:09 +0700

    test

--- MadDog.cs
+++ MadDog.cs
@@ -194,9 +194,9 @@ namespace MadDog
                         {
                             if(s.Name == "SlaveCatcherNetThrow" && actor.isAttacking)
                             {
-                                DebugWindow.LogError(s.Name);
-                                //Vector2 destination = actor.CurrentAction.Destination;
-                                //DrawLineTOPoint(new Vector3(destination.X, destination.Y, 0));
+                                //DebugWindow.LogError(s.Name);
+                                Vector2 destination = actor.CurrentAction.Destination;
+                                DrawLineTOPoint(new Vector3(destination.X, destination.Y, 0));
                             }
                             //DebugWindow.LogError(s.Name);
                         }
